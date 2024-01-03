81677e80fdc39e5679655ebef19a65f374668807
Lukasz Wiecaszek <lukasz.wiecaszek@gmail.com>
2024-01-03 17:13:11 +0100

    Update example 'gps_rft_v1'

--- examples/grc/gps_rft_v1/gps_rft_v1.py
+++ examples/grc/gps_rft_v1/gps_rft_v1.py
@@ -6,38 +6,26 @@
 #
 # GNU Radio Python Flow Graph
 # Title: gps_rft_v1
-# GNU Radio version: v3.10.5.0-1-g480f600c
+# GNU Radio version: 3.10.7.0
 
 from packaging.version import Version as StrictVersion
-
-if __name__ == '__main__':
-    import ctypes
-    import sys
-    if sys.platform.startswith('linux'):
-        try:
-            x11 = ctypes.cdll.LoadLibrary('libX11.so')
-            x11.XInitThreads()
-        except:
-            print("Warning: failed to XInitThreads()")
-
 from PyQt5 import Qt
 from gnuradio import qtgui
-from gnuradio.filter import firdes
-import sip
 from gnuradio import blocks
 import pmt
 from gnuradio import gnss
 from gnuradio import gr
+from gnuradio.filter import firdes
 from gnuradio.fft import window
 import sys
 import signal
+from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
+import sip
 
 
-
-from gnuradio import qtgui
 
 class gps_rft_v1(gr.top_block, Qt.QWidget):
 
@@ -48,8 +36,8 @@ class gps_rft_v1(gr.top_block, Qt.QWidget):
         qtgui.util.check_set_qss()
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except:
-            pass
+        except BaseException as exc:
+            print(f"Qt GUI: Could not set Icon: {str(exc)}", file=sys.stderr)
         self.top_scroll_layout = Qt.QVBoxLayout()
         self.setLayout(self.top_scroll_layout)
         self.top_scroll = Qt.QScrollArea()
@@ -69,8 +57,8 @@ class gps_rft_v1(gr.top_block, Qt.QWidget):
                 self.restoreGeometry(self.settings.value("geometry").toByteArray())
             else:
                 self.restoreGeometry(self.settings.value("geometry"))
-        except:
-            pass
+        except BaseException as exc:
+            print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
 
         ##################################################
         # Variables
@@ -84,6 +72,7 @@ class gps_rft_v1(gr.top_block, Qt.QWidget):
         ##################################################
         # Blocks
         ##################################################
+
         self.qtgui_time_sink_x_0_0_0_0_0 = qtgui.time_sink_f(
             200, #size
             1000, #samp_rate
@@ -98,7 +87,7 @@ class gps_rft_v1(gr.top_block, Qt.QWidget):
 
         self.qtgui_time_sink_x_0_0_0_0_0.enable_tags(False)
         self.qtgui_time_sink_x_0_0_0_0_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
-        self.qtgui_time_sink_x_0_0_0_0_0.enable_autoscale(False)
+        self.qtgui_time_sink_x_0_0_0_0_0.enable_autoscale(True)
         self.qtgui_time_sink_x_0_0_0_0_0.enable_grid(False)
         self.qtgui_time_sink_x_0_0_0_0_0.enable_axis_labels(True)
         self.qtgui_time_sink_x_0_0_0_0_0.enable_control_panel(False)
@@ -183,7 +172,7 @@ class gps_rft_v1(gr.top_block, Qt.QWidget):
 
         self._qtgui_time_sink_x_0_0_0_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0_0_0_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_time_sink_x_0_0_0_0_win)
-        self.gnss_type_converter_0_0_0 = gnss.fc64_to_fc32(1)
+        self.gnss_type_converter_0_0_0 = gnss.f64_to_f32(1)
         self.gnss_type_converter_0_0 = gnss.fc64_to_fc32(1)
         self.gnss_signal_normalizer_0 = gnss.signal_normalizer_s16_fc32(1, 12)
         self.gnss_rft_0 = gnss.rft()
@@ -219,29 +208,27 @@ class gps_rft_v1(gr.top_block, Qt.QWidget):
         self.gnss_acquisition_and_tracking_0_0.set_acq_params(gnss.NAVIGATION_SYSTEM_GPS, 11)
         self.gnss_acquisition_and_tracking_0 = gnss.acquisition_and_tracking(samp_rate, dll_bw_coarse, pll_bw_coarse, dll_bw_fine, pll_bw_fine)
         self.gnss_acquisition_and_tracking_0.set_acq_params(gnss.NAVIGATION_SYSTEM_GPS, 1)
-        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
-        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_short*1, '/home/memyselfandi/projects/sdr-files/gnss/2013_04_04_GNSS_SIGNAL_at_CTTC_SPAIN/2013_04_04_GNSS_SIGNAL_at_CTTC_SPAIN.dat', True, 0, 0)
+        self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_gr_complex*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_short*1, '/home/memyselfandi/projects/sdr-files/gnss/2013_04_04_GNSS_SIGNAL_at_CTTC_SPAIN/2013_04_04_GNSS_SIGNAL_at_CTTC_SPAIN.dat', False, 0, 0)
         self.blocks_file_source_0.set_begin_tag(pmt.PMT_NIL)
-        self.blocks_complex_to_imag_0 = blocks.complex_to_imag(1)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.msg_connect((self.gnss_nav_message_decoder_0, 'ephemeris'), (self.gnss_pseudoranges_decoder_0, 'ephemeris'))
         self.msg_connect((self.gnss_nav_message_decoder_0, 'clock'), (self.gnss_pseudoranges_decoder_0, 'clock'))
+        self.msg_connect((self.gnss_nav_message_decoder_0, 'ephemeris'), (self.gnss_pseudoranges_decoder_0, 'ephemeris'))
         self.msg_connect((self.gnss_nav_message_decoder_0_0, 'ephemeris'), (self.gnss_pseudoranges_decoder_0, 'ephemeris'))
         self.msg_connect((self.gnss_nav_message_decoder_0_0, 'clock'), (self.gnss_pseudoranges_decoder_0, 'clock'))
         self.msg_connect((self.gnss_nav_message_decoder_0_0_0, 'clock'), (self.gnss_pseudoranges_decoder_0, 'clock'))
         self.msg_connect((self.gnss_nav_message_decoder_0_0_0, 'ephemeris'), (self.gnss_pseudoranges_decoder_0, 'ephemeris'))
-        self.msg_connect((self.gnss_nav_message_decoder_0_0_0_0, 'ephemeris'), (self.gnss_pseudoranges_decoder_0, 'ephemeris'))
         self.msg_connect((self.gnss_nav_message_decoder_0_0_0_0, 'clock'), (self.gnss_pseudoranges_decoder_0, 'clock'))
-        self.connect((self.blocks_complex_to_imag_0, 0), (self.qtgui_time_sink_x_0_0_0_0_0, 0))
+        self.msg_connect((self.gnss_nav_message_decoder_0_0_0_0, 'ephemeris'), (self.gnss_pseudoranges_decoder_0, 'ephemeris'))
         self.connect((self.blocks_file_source_0, 0), (self.gnss_signal_normalizer_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.gnss_acquisition_and_tracking_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.gnss_acquisition_and_tracking_0_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.gnss_acquisition_and_tracking_0_0_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.gnss_acquisition_and_tracking_0_0_0_0, 0))
+        self.connect((self.blocks_throttle2_0, 0), (self.gnss_acquisition_and_tracking_0, 0))
+        self.connect((self.blocks_throttle2_0, 0), (self.gnss_acquisition_and_tracking_0_0, 0))
+        self.connect((self.blocks_throttle2_0, 0), (self.gnss_acquisition_and_tracking_0_0_0, 0))
+        self.connect((self.blocks_throttle2_0, 0), (self.gnss_acquisition_and_tracking_0_0_0_0, 0))
         self.connect((self.gnss_acquisition_and_tracking_0, 0), (self.gnss_ca_symbols_to_nav_bits_0, 0))
         self.connect((self.gnss_acquisition_and_tracking_0, 0), (self.gnss_type_converter_0_0, 0))
         self.connect((self.gnss_acquisition_and_tracking_0, 1), (self.gnss_type_converter_0_0_0, 0))
@@ -256,15 +243,15 @@ class gps_rft_v1(gr.top_block, Qt.QWidget):
         self.connect((self.gnss_nav_message_decoder_0_0, 0), (self.gnss_pseudoranges_decoder_0, 1))
         self.connect((self.gnss_nav_message_decoder_0_0_0, 0), (self.gnss_pseudoranges_decoder_0, 2))
         self.connect((self.gnss_nav_message_decoder_0_0_0_0, 0), (self.gnss_pseudoranges_decoder_0, 3))
-        self.connect((self.gnss_pseudoranges_decoder_0, 3), (self.gnss_pvt_user_0, 3))
         self.connect((self.gnss_pseudoranges_decoder_0, 2), (self.gnss_pvt_user_0, 2))
-        self.connect((self.gnss_pseudoranges_decoder_0, 1), (self.gnss_pvt_user_0, 1))
         self.connect((self.gnss_pseudoranges_decoder_0, 0), (self.gnss_pvt_user_0, 0))
+        self.connect((self.gnss_pseudoranges_decoder_0, 3), (self.gnss_pvt_user_0, 3))
+        self.connect((self.gnss_pseudoranges_decoder_0, 1), (self.gnss_pvt_user_0, 1))
         self.connect((self.gnss_pvt_user_0, 0), (self.gnss_rft_0, 0))
         self.connect((self.gnss_rft_0, 0), (self.gnss_geojson_file_sink_1, 0))
-        self.connect((self.gnss_signal_normalizer_0, 0), (self.blocks_throttle_0, 0))
+        self.connect((self.gnss_signal_normalizer_0, 0), (self.blocks_throttle2_0, 0))
         self.connect((self.gnss_type_converter_0_0, 0), (self.qtgui_time_sink_x_0_0_0_0, 0))
-        self.connect((self.gnss_type_converter_0_0_0, 0), (self.blocks_complex_to_imag_0, 0))
+        self.connect((self.gnss_type_converter_0_0_0, 0), (self.qtgui_time_sink_x_0_0_0_0_0, 0))
 
 
     def closeEvent(self, event):
@@ -280,7 +267,7 @@ class gps_rft_v1(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
+        self.blocks_throttle2_0.set_sample_rate(self.samp_rate)
 
     def get_pll_bw_fine(self):
         return self.pll_bw_fine
